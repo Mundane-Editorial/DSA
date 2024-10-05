19e306bcf226a4f771e1a66fbc1fdc8e32ae768f
Mundane-Editorial <uday2k17@rediffmail.com>
2024-10-05 23:41:05 +0530

    modified string permutations and added the results into a list for leetcode solution

--- src/Recursion/permutations.java
+++ src/Recursion/permutations.java
@@ -1,16 +1,21 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class permutations {
     public static void main(String args[])
     {
         String str = "abc";
-        allpermutations(str, "");
+        ArrayList<String> list = new ArrayList<>();
+        allpermutations(str, "", list);
+        System.out.println(list.toString());
     }
 
-    private static void allpermutations(String str, String s) {
+    private static void allpermutations(String str, String s, ArrayList<String> list) {
         if(str.length() == 0)
         {
             System.out.println(s);
+            list.add(s);
             return;
         }
         for(int i =0; i<str.length(); i++)
@@ -18,7 +23,7 @@ public class permutations {
             char ch = str.charAt(i);
 
             String newstring = str.substring(0, i) + str.substring(i+1);
-            allpermutations(newstring, s+ch);
+            allpermutations(newstring, s+ch, list);
         }
     }
 }
