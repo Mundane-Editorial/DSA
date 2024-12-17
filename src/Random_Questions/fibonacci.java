64dde42d94f83fa7a99bb4384759deba461e4ac0
Mundane-Editorial <uday2k17@rediffmail.com>
2024-12-17 13:46:16 +0530

    fibonacci practice

---
+++ src/Random_Questions/fibonacci.java
@@ -0,0 +1,21 @@
+package Random_Questions;
+
+public class fibonacci {
+    public static void printFibonacci(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printFibonacci(n);
+    }
+}
