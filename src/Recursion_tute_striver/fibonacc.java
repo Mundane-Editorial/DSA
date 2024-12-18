db5ef3bb2e2399aea7ab052280b6dd5272c88cfe
Mundane-Editorial <uday2k17@rediffmail.com>
2024-12-18 18:31:33 +0530

    multiple recursion calls using fibonacci

---
+++ src/Recursion_tute_striver/fibonacc.java
@@ -0,0 +1,20 @@
+package Recursion_tute_striver;
+
+public class fibonacc {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(getfibo(n));
+    }
+
+    private static int getfibo(int n) {
+        if(n <= 1){
+            return n;
+        }
+        return getfibo(n-1) + getfibo(n-2);
+    }
+}
+
+
+/*
+* time complexity :: ~O(2^N)  approx;
+* */
