9c327652f485acdee7da27cbd2cc9ec8740b48f1
Mundane-Editorial <uday2k17@rediffmail.com>
2024-12-21 19:01:12 +0530

    elimination game : recursion

---
+++ src/Strings/Elimination_game.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.ArrayList;
+
+public class Elimination_game {
+    public static void main(String[] args) {
+        int n = 6;
+
+        System.out.println(lastRemaining(n));
+    }
+
+    private static int lastRemaining(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i + 1);
+        }
+        return lastone(list, true);
+    }
+
+    static public int lastone(ArrayList<Integer> list, boolean isleft) {
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+
+        ArrayList<Integer> nextRound = new ArrayList<>();
+
+        for (int i = (isleft ? 1 : 0); i < list.size(); i += 2) {
+            nextRound.add(list.get(i));
+        }
+
+        return lastone(nextRound, !isleft);
+    }
+}
